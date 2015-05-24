fec3856fd710f75e19b7d0268e00894738a7e19b
ryanicle <ryanicle@gmail.com>
2015-05-24 03:25:58 +0000

    Add Card unit tests.

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,11 @@
 class Card < ActiveRecord::Base
+  validates :name, presence: true
+  validates :number, presence: true
+  validates :expired_month, presence: true
+  validates :expired_year, presence: true
+  validates :security_code, presence: true
+  validates :provider, presence: true
+  validates :interest_rate, presence: true
+  validates :category, presence: true
+  validates :image, presence: true
 end
--- test/models/card_test.rb
+++ test/models/card_test.rb
@@ -4,4 +4,23 @@ class CardTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'should not save route without name, number, expired_month, expired_year, security_code, provider, interest_rate, category, image' do
+    card = Card.new
+    assert_not  card.save
+  end
+
+  test 'should save card' do
+    card = Card.new
+    card.name = Faker::Name.name
+    card.number = Faker::Business.credit_card_number
+    card.expired_month = Faker::Number.number(1)
+    card.expired_year = 2018
+    card.security_code = Faker::Number.number(3)
+    card.provider = Faker::Business.credit_card_type
+    card.interest_rate = Faker::Number.number(1)
+    card.category = Faker::Commerce.product_name
+    card.image = Faker::Avatar.image
+    assert card.save
+  end
+
 end
